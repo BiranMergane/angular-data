6c6b0ec25b215403eefabdb6faa270a6ae473048
Biran Mergane <b.mergane2007@gmail.com>
2024-12-09 09:56:55 +0000

    inizio secondo esercizio

--- angular-data/src/app/app.component.ts
+++ angular-data/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HelloWorldComponent } from './hello-world/hello-world.component';
 import { UserItemComponent } from './user-item/user-item.component';
 import { CommonModule } from '@angular/common';
+import { UserListComponent } from './user-list/user-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, UserItemComponent, HelloWorldComponent], 
+  imports: [CommonModule, RouterOutlet, UserItemComponent, HelloWorldComponent, UserListComponent], 
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
